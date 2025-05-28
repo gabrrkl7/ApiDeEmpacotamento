d4bf7b767a3e7c94077fa5a8d3c0ffc42a09cf9a
Gabriel <gabriel.dev09@gmail.com>
2025-05-27 22:49:42 -0300

    feat: migrations

---
+++ src/LojaDoManoel.Api/Migrations/20250527223224_InitialCreate.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LojaDoManoel.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Caixas",
+                columns: table => new
+                {
+                    Nome = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Altura = table.Column<int>(type: "int", nullable: false),
+                    Largura = table.Column<int>(type: "int", nullable: false),
+                    Comprimento = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Caixas", x => x.Nome);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Altura = table.Column<int>(type: "int", nullable: false),
+                    Largura = table.Column<int>(type: "int", nullable: false),
+                    Comprimento = table.Column<int>(type: "int", nullable: false),
+                    PedidoId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Pedidos_PedidoId",
+                        column: x => x.PedidoId,
+                        principalTable: "Pedidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_PedidoId",
+                table: "Produtos",
+                column: "PedidoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Caixas");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
